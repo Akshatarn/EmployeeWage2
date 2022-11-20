1ffd006ef21c47a5d481d60f7c32fec636db0271
akshata_rn <akshatarn123@gamail.com>
2022-11-20 21:43:43 +0530

    Checks employee is Present or Absent

--- EmployeeWage2/Program.cs
+++ EmployeeWage2/Program.cs
@@ -11,6 +11,27 @@ namespace EmployeeWage2
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Welcome to Employee Computation");
+            const int EMP_RATE_HR = 20;
+            int empPresent = 1;
+            int empWage = 0;
+            int empHrs = 0;
+            Random r = new Random();
+            int empInput = r.Next(0, 2);
+            if (empPresent == empInput)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+                empHrs = 0;
+            }
+            empWage = EMP_RATE_HR * empHrs;
+            Console.WriteLine("DAily Employee wage is:" + empWage);
+            Console.ReadLine();
+
         }
     }
 }
